d714b2e288f6ba63f8be0756aed5ab0999f71069
zhongkangjk <296053096@qq.com>
2020-09-15 23:45:57 +0800

    lizi

--- weblist.py
+++ weblist.py
@@ -10,22 +10,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
-import time
+import time,os
 # driver.find_elements_by_id("cheddar")
 # driver.find_element_by_css_selector("#cheese #cheddar")
 # driver.find_elements_by_class_name()
 # driver.find_element_by_xpath()
 # driver..find_element_by_link_text("新闻")
-
-with webdriver.Chrome() as driver:
-    wait = WebDriverWait(driver, 10 , 0.5) #显式等待  until  /  until_not
-    driver.implicitly_wait(10) #隐性等待
-    driver.get("https://www.baidu.com") #发送请求
-    #driver.refresh() #刷新浏览器
-    #driver.set_window_size(1400,800) #设置浏览器大小
-    driver.maximize_window()   #浏览器窗口最大化
-    driver.find_element_by_id("kw").send_keys("selenium",Keys.ENTER)
-    #driver.find_element_by_id("su").click()
-    time.sleep(10)
-    first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "h3>div")))
-    #print(first_result.get_attribute("textContent"))
+def 例子():
+    with webdriver.Chrome() as driver:
+        wait = WebDriverWait(driver, 10 , 0.5) #显式等待  until  /  until_not
+        driver.implicitly_wait(10) #隐性等待
+        driver.get("https://www.baidu.com") #发送请求
+        #driver.refresh() #刷新浏览器
+        #driver.set_window_size(1400,800) #设置浏览器大小
+        driver.maximize_window()   #浏览器窗口最大化
+        driver.find_element_by_id("kw").send_keys("selenium",Keys.ENTER)
+        #driver.find_element_by_id("su").click()
+        time.sleep(10)
+        first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "h3>div")))
+        #print(first_result.get_attribute("textContent"))
